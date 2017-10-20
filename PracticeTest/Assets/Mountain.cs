634b12a34103af24bb4eb7f5f2528aa7fa890e7d
Troy Anderson <troy.anderson@genesisenergy.co.nz>
2017-10-20 20:10:21 +1300

    Changes

---
+++ PracticeTest/Assets/Mountain.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//Make sure you get rid of the namespace that is automatically generated here.
+class Mountain
+{
+    //List all the columns of your table here in the same format as below.
+    public int MountainID { get; set; }
+    public string MountainName { get; set; }
+    public int X { get; set; }
+    public int Y { get; set; }
+    public int Z { get; set; }
+    public int Size { get; set; }
+    public string Symbol { get; set; }
+}
---
+++ PracticeTest/Assets/Sketch.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using Pathfinding.Serialization.JsonFx; //make sure you include this using
+
+public class Sketch : MonoBehaviour
+{
+    public GameObject myPrefab;
+    public string _WebsiteURL = "http://320-labtest2.azurewebsites.net/tables/Mountain?zumo-api-version=2.0.0";
+
+    void Start()
+    {
+        //Reguest.GET can be called passing in your ODATA url as a string in the form:
+        http://320-labtest2.azurewebsites.net/tables/Mountain?zumo-api-version=2.0.0
+        //The response produce is a JSON string
+        string jsonResponse = Request.GET(_WebsiteURL);
+
+        //Just in case something went wrong with the request we check the reponse and exit if there is no response.
+        if (string.IsNullOrEmpty(jsonResponse))
+        {
+            return;
+        }
+
+        //We can now deserialize into an array of objects - in this case the class we created. The deserializer is smart enough to instantiate all the classes and populate the variables based on column name.
+        Mountain[] mountainsArray = JsonReader.Deserialize<Mountain[]>(jsonResponse);
+
+        //instantiate two variables with corresponding variable names and assigned values
+        int i = 0;
+        int length = mountainsArray.Length;
+
+        //loop through the array of objects and access each object individually + instantiate each one with specified paramters
+        foreach (Mountain mountain in mountainsArray)
+        {
+            float perc = i / (float)length;
+            float sin = Mathf.Sin(perc * Mathf.PI / 2);
+
+            //pull position data from array
+
+            //float x = 1.8f + sin * totalDistance;
+            float x = mountainsArray[i].X;
+            //float y = 5.0f;
+            float y = mountainsArray[i].Y;
+            //float z = 0.0f;
+            float z = mountainsArray[i].Z;
+
+            //set posiiton, rotation, text and size based on data in array
+
+            var newCube = (GameObject)Instantiate(myPrefab, new Vector3(x, y, z), Quaternion.identity);
+            //newCube.GetComponent<CubeScript>().SetSize(.45f * (1.0f - perc));         <-- Manual Size Setting
+            newCube.GetComponent<CubeScript>().SetSize(mountainsArray[i].Size * 0.2f);
+            //newCube.GetComponent<CubeScript>().rotateSpeed = .2f + perc * 4.0f;       <-- Manual rotation speed
+            newCube.GetComponent<CubeScript>().rotateSpeed = 0;
+            newCube.GetComponentInChildren<TextMesh>().text = mountainsArray[i].MountainName;
+            i++;
+
+
+
+            //----------------------
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
